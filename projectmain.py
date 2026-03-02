3e8153293ffb867fda522b5f7712d8596c533564
C2Escarda <escarda.gd@obmontessori.edu.ph>
2026-03-02 12:33:19 +0800

    Update projectmain.py

--- projectmain.py
+++ projectmain.py
@@ -7,12 +7,12 @@ def TeamPlays(event=None):
 
     if team == "Placeholder":
         document.querySelector("#result").innerHTML = (
-            "<br>Please select a team to find your players!"
+            "<br>Please select a team to find your players!" #you choose the placeholder and it tells you to choose an actual team
         )
         return
 
     team_players = {
-        "Blue Bears": ["AKINGSON JAKE ANDES", 
+        "Blue Bears": ["AKINGSON JAKE ANDES",  #these are the actual players of blue bears.
                        "ALEXANDER TACAN", "ALEXI VILLANUEVA", "ASHLEY KIRSTEN Y. SANTOS", "AUDREY ANNE DUMAGUING", "AURORA ISHTAR A. UBANA", "CYRENNE MENDEZ", "DANIELLE ALEENA NOBLE", "FIL AYALA", "GABRIELLE L. DAMONDAMON", "GIOVANNI ESCARDA", "JARIX ZALES", "JOHN ALFRED F. TARUC JR.", "JOHN LIGAS", "JOSEPH DERAY", "KURT TENORIO", "MARGAUX ALAINA FERRER", "MARLEY SUMMER MANESE", "MARTINA CABRILLOS", "MARY BERNADETTE DAED", "MIKE JARED DE JESUS", "NATHAN MARI ANTHONY DEVERA GRANDE", "NATHAN TIONGSON", "PAULINE ECRAEALA", "RISO SALAPUNEN", "VIC GOROM", "ZION MARGARET FABUL"],
         "Red Bulldogs": ["TO BE ANNOUNCED"],
         "Yellow Tigers": ["TO BE ANNOUNCED"],
@@ -24,5 +24,6 @@ def TeamPlays(event=None):
         players_html += player + "<br>"
 
     document.querySelector("#result").innerHTML = (
-        f"<b>Team:</b> <b>{team}</b><br><b>Players:</b><br> {players_html}"
+        f"<b>Team:</b> <b>{team}</b><br><b>Players:</b><br> {players_html}" #this iterates every player in the blue bears team and displays it
+
     )
